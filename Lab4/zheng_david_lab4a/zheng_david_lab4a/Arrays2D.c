8621d4241bd34edfae177708081fb0a798dc2819
David Zheng <davidzheng@knights.ucf.edu>
2018-09-24 23:07:05 -0400

    fixed lab 4 and added some comments

--- Lab4/zheng_david_lab4a/zheng_david_lab4a/Arrays2D.c
+++ Lab4/zheng_david_lab4a/zheng_david_lab4a/Arrays2D.c
@@ -8,9 +8,9 @@
 
 #include <stdio.h>
 
-int ROW, COL;
+int ROW, COL; // define ROW and COL
 
-// creates prototypes of functions
+// create prototypes
 void max(int arr[ROW][COL]);
 void displayOutputs(int arr[ROW][COL]);
 void rowSum(int arr[ROW][COL]);
@@ -19,24 +19,24 @@ void isSquare(int ROW, int COL);
 
 int main(int argc, const char * argv[]) {
     puts("Let's make a 2D array.\nHow many rows are in your array?");
-    scanf("%d", &ROW);
-    while(ROW <= 0) {
+    scanf("%d", &ROW); // take in value for ROW
+    while(ROW <= 0) { // checks that there is at least 1 row
         puts("You need to have at least 1 row.\nHow many rows are in your array?");
         scanf("%d", &ROW);
     }
     puts("How many columns are in your array?");
-    scanf("%d", &COL);
-    while(COL <= 0) {
+    scanf("%d", &COL); // take in value for COL
+    while(COL <= 0) { // checks that there is at least 1 column
         puts("You need to have at least 1 column.\nHow many columns are in your array?");
         scanf("%d", &COL);
     }
-    int arr[ROW][COL];
-    for(size_t r = 0; r < ROW; ++r) {
-        for(size_t c = 0; c < COL; ++c) {
+    int arr[ROW][COL]; // creates a 2d array with ROW rows and COL columns
+    for(size_t r = 0; r < ROW; ++r) { // for every row
+        for(size_t c = 0; c < COL; ++c) { // for every element in every row
             printf("enter\t[%zu][%zu]:\t", r, c);
             int value;
-            scanf("%d", &value);
-            arr[r][c] = value;
+            scanf("%d", &value); // take in value
+            arr[r][c] = value; // put value in array
         }
     }
     printf("\n");
@@ -51,11 +51,11 @@ int main(int argc, const char * argv[]) {
 }
 
 void max(int arr[ROW][COL]) {
-    int max = -1;
+    int max = arr[0][0]; // set the max value to first element in array
     for(size_t i = 0; i < ROW; ++i) {
-        for(size_t j = 0; j < COL; ++j) {
-            if(arr[i][j] > max || max == -1) {
-                max = arr[i][j];
+        for(size_t j = 0; j < COL; ++j) { // for every element in array
+            if(arr[i][j] > max) { // if the current element is greater than max
+                max = arr[i][j]; // make the current element max
             }
         }
     }
@@ -63,10 +63,10 @@ void max(int arr[ROW][COL]) {
 }
 
 void rowSum(int arr[ROW][COL]) {
-    for(size_t i = 0; i < ROW; ++i) {
-        int sum = 0;
-        for(size_t j = 0; j < COL; ++j) {
-            sum += arr[i][j];
+    for(size_t i = 0; i < ROW; ++i) { // for every row
+        int sum = 0; // sum starts at 0
+        for(size_t j = 0; j < COL; ++j) { // for every element in current row
+            sum += arr[i][j]; // add current element value to sum
         }
         size_t row = i + 1;
         printf("The sum of row %zu = %d.\n", row, sum);
@@ -75,10 +75,10 @@ void rowSum(int arr[ROW][COL]) {
 }
 
 void columnSum(int arr[ROW][COL]) {
-    for(size_t i = 0; i < COL; ++i) {
-        int sum = 0;
-        for(size_t j = 0; j < ROW; j++) {
-            sum += arr[j][i];
+    for(size_t i = 0; i < COL; ++i) { // for every column
+        int sum = 0; // sum starts at 0
+        for(size_t j = 0; j < ROW; j++) { // for every element in current column
+            sum += arr[j][i]; // add current element to sum
         }
         size_t col = i + 1;
         printf("The sum of column %zu = %d.\n", col, sum);
@@ -88,7 +88,7 @@ void columnSum(int arr[ROW][COL]) {
 
 
 void isSquare(int ROW, int COL) {
-    if(ROW == COL) {
+    if(ROW == COL) { // if there are as many rows as there are columns
         printf("The array is square because there are %d rows and %d columns.\n", ROW, COL);
     }
     else {
@@ -99,13 +99,13 @@ void isSquare(int ROW, int COL) {
 
 void displayOutputs(int arr[ROW][COL]) {
     puts("Your new array is:");
-    for(size_t i = 0; i < ROW; ++i) {
-        printf("[");
-        for(size_t j = 0; j < COL - 1; ++j) {
-            printf("%d,\t", arr[i][j]);
+    for(size_t i = 0; i < ROW; ++i) { // for every row
+        printf("["); // open bracket
+        for(size_t j = 0; j < COL - 1; ++j) { // for every element in row excluding the last one
+            printf("%d,\t", arr[i][j]); // print out element with a comma at the end
         }
-        printf("%d", arr[i][COL - 1]);
-        printf("]\n");
+        printf("%d", arr[i][COL - 1]); // print out last element
+        printf("]\n"); // closing bracket
     }
     printf("\n");
 }
--- Lab4/zheng_david_lab4b/zheng_david_lab4b/ArraysofPointers.c
+++ Lab4/zheng_david_lab4b/zheng_david_lab4b/ArraysofPointers.c
@@ -9,7 +9,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int students, exams;
+int students, exams; // define students and exams
+
+// create prototypes
 void printArray(int grades[students][exams]);
 void minimum(int grades[students][exams]);
 void maximum(int grades[students][exams]);
@@ -18,68 +20,68 @@ void end(int grades[students][exams]);
 
 int main(int argc, const char * argv[]) {
     puts("Let's make a 2D array of students and their exam grades.\nHow many students are there?");
-    scanf("%d", &students);
-    while(students <= 0) {
+    scanf("%d", &students); // takes in number of students
+    while(students <= 0) { // checks that there is at least 1 student
         puts("You need to have at least 1 student.\nHow many students are there?");
         scanf("%d", &students);
     }
     puts("How many exams did each student take?");
-    scanf("%d", &exams);
-    while(exams <= 0) {
+    scanf("%d", &exams); // takes in number of exams
+    while(exams <= 0) { // checks that there is at least 1 test
         puts("Each student should have taken at least 1 exam.\nHow many exams did each student take?");
         scanf("%d", &exams);
     }
-    int studentGrades[students][exams];
-    int grade;
-    for(size_t s = 0; s < students; ++s) {
-        for(size_t e = 0; e < exams; ++e) {
+    int studentGrades[students][exams]; // creates a 2d array of students and their test scores
+    int grade; // defines variable int grade
+    for(size_t s = 0; s < students; ++s) { // for every student
+        for(size_t e = 0; e < exams; ++e) { // for every test each student has taken
             printf("Grade of Exam %zu for Student %zu:\t", e + 1, s + 1);
-            scanf("%d", &grade);
-            while(grade < 0 || grade > 100) {
+            scanf("%d", &grade); // takes in grade for each test
+            while(grade < 0 || grade > 100) { // checks that grades are between 0 and 100
                 printf("Grades are only between 0 and 100. Please try again.\nGrade of Exam %zu for Student %zu:\t", e + 1, s + 1);
                 scanf("%d", &grade);
             }
             if(grade >= 0 && grade <= 100) {
-                studentGrades[s][e] = grade;
+                studentGrades[s][e] = grade; // puts the grade into the array
             }
         }
     }
     printf("\n");
-    void (*f[5])(int studentGrades[students][exams]) = {printArray, minimum, maximum, average, end};
-    printf("Enter a choice:\n\t0\tPrint the array of grades\n\t1\tFind the minimumgrade\n\t2\tFind the maximum grade\n\t3\tPrint the average on all tests for each student\n\t4\tEnd Program\nYour choice:\t");
-    int choice;
-    scanf("%d", &choice);
-    if(choice >=5 || choice < 0) {
-        printf("Your choice was not an option.\nEnter a choice:\n\t0\tPrint the array of grades\n\t1\tFind the minimumgrade\n\t2\tFind the maximum grade\n\t3\tPrint the average on all tests for each student\n\t4\tEnd Program\nYour choice:\t");
+    void (*f[5])(int studentGrades[students][exams]) = {printArray, minimum, maximum, average, end}; // initialize an array of 5 pointers to functions that take an int argument and returns void
+    printf("Enter a choice:\n\t0\tPrint the array of grades\n\t1\tFind the minimum grade\n\t2\tFind the maximum grade\n\t3\tPrint the average on all tests for each student\n\t4\tEnd Program\nYour choice:\t");
+    int choice; // define variable choice
+    scanf("%d", &choice); // takes in user input for choice
+    if(choice >=5 || choice < 0) { // checks that the choice is valid (between 0 and 5
+        printf("Your choice was not an option.\nEnter a choice:\n\t0\tPrint the array of grades\n\t1\tFind the minimum grade\n\t2\tFind the maximum grade\n\t3\tPrint the average on all tests for each student\n\t4\tEnd Program\nYour choice:\t");
         scanf("%d", &choice);
     }
-    while(choice >= 0 && choice < 5) {
+    while(choice >= 0 && choice < 5) { // if choice is valid
         printf("\n");
-        (*f[choice])(studentGrades);
-        printf("Enter a choice:\n\t0\tPrint the array of grades\n\t1\tFind the minimumgrade\n\t2\tFind the maximum grade\n\t3\tPrint the average on all tests for each student\n\t4\tEnd Program\nYour choice:\t");
-        scanf("%d", &choice);
+        (*f[choice])(studentGrades); // invoke function at location choice in array f and pass studentGrades as an argument
+        printf("Enter a choice:\n\t0\tPrint the array of grades\n\t1\tFind the minimum grade\n\t2\tFind the maximum grade\n\t3\tPrint the average on all tests for each student\n\t4\tEnd Program\nYour choice:\t");
+        scanf("%d", &choice); // asks for another choice after function is ran
     }
     
 }
 
 void printArray(int grades[students][exams]) {
     printf("Your Students' Grades:\n");
-    for(size_t i = 0; i < students; ++i) {
-        printf("\nstudent %zu grades:\t[", i + 1);
-        for(size_t j = 0; j < exams - 1; ++j) {
-            printf("%d,\t", grades[i][j]);
+    for(size_t i = 0; i < students; ++i) { // for every student
+        printf("\nstudent %zu grades:\t[", i + 1); // print student # with index 0 being student 1 and so on
+        for(size_t j = 0; j < exams - 1; ++j) { // for every exam the current student has taken excluding last one
+            printf("%d,\t", grades[i][j]); // print out the grade and comma separator
         }
-        printf("%d]", grades[i][exams - 1]);
+        printf("%d]", grades[i][exams - 1]); // print out last grade with closing bracket
     }
     printf("\n\n");
 }
 
 void minimum(int grades[students][exams]) {
-    int lowGrade = 100;
-    for(size_t i = 0; i < students; ++i) {
-        for(size_t j = 0; j < exams; ++j) {
-            if(grades[i][j] < lowGrade) {
-                lowGrade = grades[i][j];
+    int lowGrade = 100; // set the lowest grade value to maximum possible grade
+    for(size_t i = 0; i < students; ++i) { // for every student
+        for(size_t j = 0; j < exams; ++j) { // for every test they have taken
+            if(grades[i][j] < lowGrade) { // if the current test is less than the lowGrade
+                lowGrade = grades[i][j]; // change lowGrade to the current test grade
             }
         }
     }
@@ -87,11 +89,11 @@ void minimum(int grades[students][exams]) {
 }
 
 void maximum(int grades[students][exams]) {
-    int highGrade = 0;
-    for(size_t i = 0; i < students; ++i) {
-        for(size_t j = 0; j < exams; ++j) {
-            if(grades[i][j] > highGrade) {
-                highGrade = grades[i][j];
+    int highGrade = 0; // set the highest grade value to the lowest possible grade
+    for(size_t i = 0; i < students; ++i) { // for every student
+        for(size_t j = 0; j < exams; ++j) { // for every exam they have taken
+            if(grades[i][j] > highGrade) { // if the current exam grade is greater than highGrade
+                highGrade = grades[i][j]; // make the highGrade the current test grade
             }
         }
     }
@@ -99,12 +101,12 @@ void maximum(int grades[students][exams]) {
 }
 
 void average(int grades[students][exams]) {
-    for(size_t i = 0; i < students; ++i) {
-        int total = 0;
-        for(size_t j = 0; j < exams; ++j) {
-            total += grades[i][j];
+    for(size_t i = 0; i < students; ++i) { // for every student
+        int total = 0; // set the sum of test grades to 0
+        for(size_t j = 0; j < exams; ++j) { // for every test taken by current student
+            total += grades[i][j]; // add the current test score to the sum of test grades
         }
-        double avg = total / exams;
+        double avg = total / exams; // defines average as sum of grades divided by number of exams
         printf("The average grade for student %zu is %.2f.\n", i + 1, avg);
     }
     printf("\n");
@@ -112,5 +114,5 @@ void average(int grades[students][exams]) {
 
 void end(int grades[students][exams]) {
     printf("Goodbye.");
-    exit(0);
+    exit(0); // exits the program
 }
