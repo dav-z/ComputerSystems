f2638b358128f89d8b097ecf3c2d78ad9171564e
David Zheng <davidzheng@knights.ucf.edu>
2018-10-11 13:49:13 -0400

    lab 6 upload

---
+++ Lab6/zheng_david_lab6/zheng_david_lab6/manual.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  zheng_david_lab6
+//
+//  Created by David Zheng on 10/4/18.
+//  Copyright © 2018 David Zheng. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
